ed6c224bce456a7c495f6445c3ce48283ee45dab
Mansoor Basha Bellary <mbellary@chewy.com>
2018-10-08 22:26:20 -0400

    feat(temp22): adding new variable temp22

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 module.exports = function() {
   console.log('Hi');
   const temp = "hi";
-  console.log(temp);
-  const temp2 = "hi";
-  console.log(temp2);
-  const temp3 = "hi";
-  console.log(temp3);
-  const temp4 = "hi";
-  console.log(temp4);
-  const temp5 = "hi";
-  console.log(temp5);
-  const temp6 = "hi";
-  console.log(temp6);
-  const temp9 = "hi";
-  console.log(temp9);
 };

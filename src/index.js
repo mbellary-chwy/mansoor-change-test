fadd867ebfd3af9bf2202da52db9864f00c7491a
Mansoor Basha Bellary <mbellary@chewy.com>
2018-10-08 20:43:08 -0400

    refactor(temp15): adding new variable temp15

--- src/index.js
+++ src/index.js
@@ -18,6 +18,6 @@ module.exports = function() {
   console.log(temp10);
   const temp13 = "hi";
   console.log(temp13);
-  const temp14 = "hi";
-  console.log(temp14);
+  const temp15 = "hi";
+  console.log(temp15);
 };
